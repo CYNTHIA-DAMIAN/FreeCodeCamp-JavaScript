fb9e68ad74461c218ee42806ebba24c7a52f830c
CYNTHIA-DAMIAN <cynthianduka97@gmail.com>
2024-04-19 18:57:47 +0100

    My first real commit

---
+++ pyramidGenerator/script.js
@@ -0,0 +1,5 @@
+let character = "Hello"; //strings
+let count = 8; //numbers
+let rows = ["Naomi", "Quincy", "CamperChan"]; //arrays
+rows[2] = 10; //arrays are mutable
+rows[rows.length - 1 ] = 10; // using  "length - 1" this gets the last element in an array 
